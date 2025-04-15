62859755842f2e087f20721c1a2c7dfd17171d56
Jiaxi Lin <jiaxi.dev@gmail.com>
2025-04-16 00:11:36 +0800

    add export for history

--- trakt2letterboxd.py
+++ trakt2letterboxd.py
@@ -4,10 +4,12 @@
 import sys
 import json
 import csv
+import os
+
 from datetime import datetime
 from dotenv import load_dotenv
-import os
 from urllib.request import Request, urlopen
+from itertools import count
 
 load_dotenv()
 
@@ -17,8 +19,8 @@ API_KEY = os.getenv("API_KEY")
 
 # if args missing, prompt input
 
-user = 'loljiaxi'
-listType = 'watchlist'
+user = input()
+list_type = input()
 
 headers = {
     'Content-Type': 'application/json',
@@ -27,48 +29,89 @@ headers = {
 }
 
 # urls
-# history = https://api.trakt.tv/users/loljiaxi/history/movies
+# history = https://api.trakt.tv/users/loljiaxi/history/movies?page=1
 # watchlist = https://api.trakt.tv/users/loljiaxi/watchlist/movies/added
 
-r = Request('https://api.trakt.tv/users/loljiaxi/watchlist/movies/added', headers=headers)
+new_list = []
+field_names = []
+file_name = ""
 
-response_body = urlopen(r).read()
+match list_type:
+    case "watchlist":
+        print("generating watchlist")
 
-newlist = []
+        file_name = 'watchlist.csv'
+        field_names = ['tmdbID', 'Title', 'Year']
 
-if response_body:
-    # create csv file
+        r = Request(f'https://api.trakt.tv/users/{user}/watchlist/movies/added', headers=headers)
+        response_body = urlopen(r).read()
 
-    data = json.loads(response_body)
+        if response_body:
+            data = json.loads(response_body)
 
-    # loop through list
-    for movie in data:
-        # check if movie already exists in array
-        # exists = 'false'
+            # loop through list
+            for movie in data:
+                newObj = {
+                    'tmdbID': movie["movie"]["ids"]["tmdb"],
+                    'Title': movie["movie"]["title"],
+                    'Year': movie["movie"]["year"]
+                }
 
-        newObj = {
-            'tmdbID': movie["movie"]["ids"]["tmdb"],
-            'Title': movie["movie"]["title"],
-            'Year': movie["movie"]["year"],
-            'WatchedDate': ''
-        }
+                # add row to csv
+                new_list.append(newObj)
 
-        # add row to csv
-        newlist.append(newObj)
+        else:
+            print("err")
 
-    with open('watchlist.csv', 'w', newline='') as csvfile:
-        fieldnames = ['tmdbID', 'Title', 'Year', 'WatchedDate']
+    case "history":
+        print("generating history")
 
-        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-        writer.writeheader()
-        writer.writerows(newlist)
+        file_name = 'history.csv'
+        field_names = ['tmdbID', 'Title', 'Year', 'Rewatch', 'WatchedDate']
+        pageLength = 10
+
+        # infinite loop for dynamic pagination
+        for currPage in count(0):
+            r = Request(f'https://api.trakt.tv/users/{user}/history/movies?page=' + str(currPage + 1), headers=headers)
+            response_body = urlopen(r).read()
+            
+            if response_body:
+                print(f"data page: {currPage + 1}")
+                data = json.loads(response_body)
+
+                # if arr empty
+                if not data:
+                    break
+
+                for movie in data:
+                    newObj = {
+                        'tmdbID': movie["movie"]["ids"]["tmdb"],
+                        'Title': movie["movie"]["title"],
+                        'Year': movie["movie"]["year"],
+                        'Rewatch': 0,
+                        'WatchedDate': movie["watched_at"]
+                    }
+
+                    # add row to csv
+                    new_list.insert(0, newObj)
         
-    
-    print(newlist)
+        unique = []
+        for movie in new_list:
+            if movie['tmdbID'] in unique:
+                movie['Rewatch'] = 1
+            else:
+                unique.append(movie['tmdbID'])
 
-    # save csv
-else:
-    print("err")
+
+# create csv file
+with open(file_name, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames = field_names)
+    writer.writeheader()
+    writer.writerows(new_list)
+
+print("list saved:", file_name)
+    
+# print(new_list)
 
 # watchlist csv format
 # tmdbID,Title,Year
